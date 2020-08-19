7c324d361416427a5588668b8abc40ddc0b29d8b
Devin Castro <dcrocker21@gmail.com>
2020-08-19 14:04:49 -0700

    basic functionality

---
+++ public/tables.js
@@ -0,0 +1,20 @@
+
+axios.get('/api/tables')
+  .then(({ data }) => {
+    console.log(data)
+    data.forEach(table => {
+      let itemElem = document.createElement('li')
+      itemElem.innerHTML = `
+      <div>
+        <p>
+          Name: ${table.name}
+          Email: ${table.email}
+          Phone: ${table.phone}
+          ID: ${table.id}
+        </p>   
+      </div>
+      `
+      document.getElementById('tables').append(itemElem)
+    })
+  })
+  .catch(err => console.error(err))
